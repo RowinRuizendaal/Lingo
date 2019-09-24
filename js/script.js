d6a7a761e24c323babc0c90fbc0198ab1587ac73
Rowin <Rowin_ruizendaal@hotmail.com>
2019-09-24 16:05:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ var bingoSheet = [
 function setup() {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < woordLength; j++) {
-            board[i][j] = '';
         }
+            //Wat wil je doen met deze variable J & I ?
     }
     console.log(board);
 }
