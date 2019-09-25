52deab970ec3f6048fd0789e2ef739de3ee51d12
Sam de Man <47493725+Firefox3000@users.noreply.github.com>
2019-09-25 11:53:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-let woordLength = 5;
-var board = [];
+var woordLength = 5;
+
+let availableNumb = [];
+
+var board = [''];
 var bingoSheet = [
     ['','','','','',],
     ['','','','','',],
@@ -10,13 +13,32 @@ var bingoSheet = [
 
 function setup() {
     for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < woordLength; j++) {
+        for (let j = 0; j < 5; j++) {
+        // board = [i[j]];
+        console.log(j);
+        // board[i].push('a', 'b');
         }
-        var board = [i + j] //Wat wil je doen met deze variable J & I ?
-        board = [i[j]];
-        board = [i][j];
+        board.push('a', 'b');
     }
     console.log(board);
+
+    for (let i = 0; i < 99; i++) {
+        availableNumb[i] = i + 1;
+    }
+    console.log(availableNumb);
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            let index = Math.floor(Math.random() * availableNumb.length);
+            let num = availableNumb[index];
+            bingoSheet[j][i] = num;
+            availableNumb.splice(index, 1);
+        }
+    }
+    console.log(bingoSheet);
 }
 
 setup();
+
+
+
